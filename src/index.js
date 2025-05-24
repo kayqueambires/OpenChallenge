e3ee55c6f230f011b3920c9acd44f92dc08f3a4e
kayqueambires <kayqueasilveira@gmail.com>
2025-05-24 19:27:24 -0300

    feat:

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 API ready on http://localhost:${PORT}`));
