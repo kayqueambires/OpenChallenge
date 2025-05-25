0bac686063ca985f507e45119fbf37ce141f1473
Gustavo <devgustavogomes31@gmail.com>
2025-05-25 16:58:14 -0300

    Pagina Cadastro

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,74 @@
+
+
+
+export default function LoginForm({ onSwitchToRegister }) {
+    return (
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm/6 font-medium  text-left text-slate-200"
+                  >
+                    Endereço de Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Email"
+                      required
+                      autoComplete="email"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm/6 font-medium text-slate-200"
+                    >
+                      Entre
+                    </label>
+                    <div className="text-sm">
+                      <a
+                        href="#"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                      >
+                        Esqueceu a senha?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      placeholder="Senha"
+                      required
+                      autoComplete="current-password"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div className='flex justify-between'>
+                  <button
+                    type="button"
+                    onClick={onSwitchToRegister}
+                    className="  basis-45 rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-slate-100 shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"             
+                  >
+                    Cadastrar
+                  </button>
+                  <button
+                    type="submit"
+                    className="  basis-45 rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-slate-100 shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Entrar
+                  </button>
+                </div>
+              </form>
+            );
+            
+}
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head';
 import AnimatedBackground from '@/components/AnimatedBackground';
-import { Rajdhani } from 'next/font/google';
+import LoginForm from '@/components/LoginForm';
+import { useState } from 'react';
+import RegisterForm from '@/components/RegisterForm';
 
-const rajdhani = Rajdhani({
-  weight: ['300', '400', '500', '600', '700'], // Escolha os pesos que você vai usar para otimização
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-rajdhani', // Defina uma variável CSS para usar com Tailwind
-});
 
 export default function Home() {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+  const handleSwitchToRegister = () => {
+    setShowLoginForm(false);
+  };
+
+  const handleSwitchToLogin = () => {
+    setShowLoginForm(true);
+  };
   return (
     <div className="min-h-screen  flex items-center justify-center">
       <Head>
@@ -22,85 +26,35 @@ export default function Home() {
         />
       </Head>
       <AnimatedBackground />
-      <main className="text-center p-8 shadow-lg rounded-lg bg-gray-800 relative z-10 flex flex-col ">
-        <h1 className="text-5xl font-[orbitron] text-indigo-700 mb-4">
-          Bem-vindo ao OpenChallenge!
-        </h1>
-        <p className="text-lg text-slate-200 mb-6">
-          Sua plataforma para desafios de programação e gamificação.
-        </p>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="/images/Logo.png"
-              className="mx-auto h-35 w-auto"
-            />
-            <h2 className="mt-4 text-center text-2xl/9  font-mono tracking-tight text-slate-200">
-              Entre na sua conta
-            </h2>
-          </div>
+      <main >
+        <div id='Forms' className='text-center p-8 shadow-lg rounded-lg bg-gray-800 relative z-10 flex flex-col '> 
+          <h1 className="text-5xl font-[orbitron] text-indigo-700 mb-4">
+            Bem-vindo ao OpenChallenge!
+          </h1>
+          <p className="text-lg text-slate-200 mb-6">
+            Sua plataforma para desafios de programação e gamificação.
+          </p>
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img
+                alt="Your Company"
+                src="/images/Logo.png"
+                className="mx-auto h-35 w-auto"
+              />
+              <h2 className="mt-4 text-center text-2xl/9  font-mono tracking-tight text-slate-200">
+                {showLoginForm ? (
+                  'Entre na sua Conta'
+                ) : ('Registre-se')}
+              </h2>
+            </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium  text-left text-slate-200"
-                >
-                  Endereço de Email
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm/6 font-medium text-slate-200"
-                  >
-                    Entre
-                  </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Esqueceu a senha?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="Senha"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full  justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-slate-100 shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Entrar
-                </button>
-              </div>
-            </form>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                {showLoginForm ? (
+                  <LoginForm onSwitchToRegister={handleSwitchToRegister} />
+                      ) : (
+                  <RegisterForm onSwitchToLogin={handleSwitchToLogin} />
+                 )}
+            </div>
           </div>
         </div>
       </main>
